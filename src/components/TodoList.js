c4af80886c569c1c29bf9460dd9df532332c96d3
abiola <abiola.arobieke@gmail.com>
2023-05-18 12:04:41 -0400

    Modify TodoList.js file

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,21 @@
 import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
 
-const TodosList = ({ todosProps }) => (
+const TodosList = ({
+  todosProps, handleCheckboxes, delTodos, setUpdate,
+}) => (
   <ul>
     {todosProps.map((todo) => (
-      <li key={todo.id}>{todo.title}</li>
+      <TodoItem
+        key={todo.id}
+        itemProp={todo}
+        delTodo={delTodos}
+        handleCheckbox={handleCheckboxes}
+        setUpdate={setUpdate}
+      />
     ))}
   </ul>
 );
-
 TodosList.propTypes = {
   todosProps: PropTypes.arrayOf(
     PropTypes.shape({
@@ -16,5 +24,8 @@ TodosList.propTypes = {
       completed: PropTypes.bool,
     }),
   ).isRequired,
+  handleCheckboxes: PropTypes.func.isRequired,
+  delTodos: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
 };
 export default TodosList;
