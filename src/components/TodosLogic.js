871d2271ef0238b6565554a5418330622f6573a6
abiola <abiola.arobieke@gmail.com>
2023-05-18 12:03:12 -0400

    Refactor TodoLogic.js file

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import TodosList from './TodoList';
+import InputTodo from './InputTodo';
 
 const TodosLogic = () => {
-  const todos = [
+  const [todos, setTodo] = useState([
     {
       id: 1,
       title: 'Watch football match',
@@ -17,12 +20,55 @@ const TodosLogic = () => {
       title: 'Learn react',
       completed: false,
     },
-  ];
+  ]);
+
+  const handleCheckbox = (id) => {
+    setTodo((prevState) => prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    }));
+  };
+
+  const delTodo = (id) => {
+    setTodo([...todos.filter((todo) => todo.id !== id)]);
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: Number(uuidv4()),
+      title,
+      completed: false,
+    };
+    setTodo([...todos, newTodo]);
+  };
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodo(todos.map((todo) => {
+      if (todo.id === id) {
+        // eslint-disable-next-line no-param-reassign
+        todo.title = updatedTitle;
+      }
+      return todo;
+    }));
+  };
 
   return (
-    <ul>
-      <TodosList todosProps={todos} />
-    </ul>
+    <>
+      <InputTodo addTodoItem={addTodoItem} />
+      <ul>
+        <TodosList
+          todosProps={todos}
+          handleCheckboxes={handleCheckbox}
+          delTodos={delTodo}
+          setUpdate={setUpdate}
+        />
+      </ul>
+    </>
   );
 };
 export default TodosLogic;
